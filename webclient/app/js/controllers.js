b511eb545043bf93689824f531b6a77a375e9bc4
Bjorn Tegelund <b.tegelund@gmail.com>
2013-11-04 22:07:49 +0900

    Added dietary profiles, users

--- webclient/app/js/controllers.js
+++ webclient/app/js/controllers.js
@@ -6,19 +6,38 @@ function TestController($scope, $location, Nutrition, Phridge) {
 	var phridgeId = $location.search()['id']; // Gets the fridge ID from the URL
 	var userId = $location.search()['user'] || 'bjorn';
 
+	var dietary_profiles = {
+		'lactose intolerant': {
+			'name': 'Lactose Intolerant',
+			'checks': [
+			function(aggrs) { return (aggrs['calcium'] > 100 ? 
+			                          "<span><icon class='icon-thumbs-down icon-2x' style='vertical-align: middle;'></i> </span><span><strong>" + $scope.name + ",</strong> that was a poor dietary choice. You don't need any more <b>milk</b> today.</span>" : '')}
+			]
+		},
+		'diabetic': {
+			'name': 'Diabetic', 
+			'checks': [
+			function(aggrs) { return ($scope.carbohydrates > 100 ? 
+				"<span><icon class='icon-thumbs-down icon-2x' style='vertical-align: middle;'></i> </span><span><strong>" + $scope.name + ",</strong> that was a poor dietary choice. You don't need any more <b>sugar</b> today.</span>": '')}
+			]
+		}
+	};
 
 	var profiles = {
-		'bjorn':{'name':'Bjorn', 
-			},
-		'thomas':{'name':'Thomas',
-			},
-	}
+		'bjorn':
+		{'name':'Bjorn', 
+		'dietary_profile': dietary_profiles['diabetic']},
+		'thomas':
+		{'name':'Thomas',
+		'dietary_profile': dietary_profiles['lactose intolerant']}
+	};
+
+	$scope.name = profiles[userId].name;
+	$scope.dietary_profile = profiles[userId]['dietary_profile'];
 
 	$scope.dailyCalories = 2000;
-	$scope.name = profiles[userId].name;
-	$scope.info = '<h3>Welcome, ' + $scope.name + '</h3><p>Here is your progress towards today\'s daily recommended intake.</p>'; // Text in the info box in the upper part of the screen
 	$scope.boxClass = ""; // Sets the display class of the info box in the upper part of the screen.
-      	$scope.consumedHtml = "You recently consumed: ";
+	$scope.consumedHtml = "You recently consumed: ";
 	/* Main values (displayed different from the rest) and related regex to extract them */
 	var mainValues = [
 	{attr: 'protein', regex: /protein/i}, 
@@ -34,11 +53,30 @@ function TestController($scope, $location, Nutrition, Phridge) {
 		$scope.consumed = Phridge.history(phridgeId).then(function (items) {
 			return _.chain(items)
 			.filter(function (item) { 
-			    return (item.removal != 0 && item.title != "placeholder"); })
+				return (item.removal != 0 && item.title != "placeholder"); })
 			.filter(function (item) { return ($scope.date - new Date(item.timein) < 1000*60*60*24); })
 			.map(function (item) { item.values = Nutrition.get(item.title); return item; })
 			.value();
 		});
+	};
+
+	$scope.recommend = function (aggregates) {
+		for (var i = $scope.dietary_profile.checks.length - 1; i >= 0; i--) {
+			var res = $scope.dietary_profile.checks[i](aggregates);
+			if(res != '') {
+				$scope.boxClass = "alert alert-danger";
+				return res;
+			}
+		};
+		return '';
+	};
+
+	$scope.getInfo = function (consumed, aggregates) {
+		var recommendation = $scope.recommend(aggregates);
+		if(recommendation == '')
+			return  '<h3>Welcome, ' + $scope.name + '</h3><p>Here is your progress towards today\'s daily recommended intake.</p><strong>Dietary Profile:</strong><span> ' + $scope.dietary_profile.name + '</span>';
+		else
+			return recommendation;
 	};
 
 	/* Generates aggregates from the fetched items from the fridge couples with the Wolfram|Alpha data */
@@ -78,48 +116,19 @@ function TestController($scope, $location, Nutrition, Phridge) {
 			_.chain(items)
 			.each(function(item) { item.values.then(extractValues) });
 		});
-		
+
 		console.log("Getting aggregates...");
-		$scope.aggregates = {}
+		$scope.aggregates = {};
 		$scope.aggregates = aggregates;
 	};
 
 	$scope.getConsumed();
 	$scope.getAggregates();
 
-    $scope.hideMini = ($scope.consumed == undefined || $scope.consumed.then(function(items) { return items.length == 0; }));
-    //$scope.hideMini = true;
-    
-	/* Not done - for recommending which item the user should take out */
-	$scope.recommend = function () {
-		$scope.info = Phridge.current(phridgeId).then(function (items) {
-			console.log(items);
-			_.chain(items)
-			.filter(function (item) {
-				Nutrition.get(item.title).then(function (values) {
-					console.log(values);
-				});
-				return 1;
-			})
-			.value();
-		});
-		// if(user picked a good item)
-		if(false) {
-			$scope.boxClass = "alert alert-success";
-			$scope.info = "<span><icon class='icon-thumbs-up icon-2x' style='vertical-align: middle;'></i> </span><span><strong>Good job, " + $scope.name + "!</strong> <b>Milk</b> is exactly what you need, as you're low on <b>calcium</b> today.</span>"	
-		}
-		// if(user picked a bad item)
-		if(true) {
-			$scope.boxClass = "alert alert-danger";
-			$scope.info = "<span><icon class='icon-thumbs-down icon-2x' style='vertical-align: middle;'></i> </span><span><strong>Hey, " + $scope.name + "!</strong> <b>Orange juice</b> is a poor dietary choice. You don't need any more <b>carbohydrates</b> or <b>Vitamin C</b> today. How about some <b>milk</b> to get that extra <b>calcium</b>?</span>"	
-		}
-		// if(user opened door but didnt take anything out yet, or simply asked for a recommendation)
-		if(false)
-			$scope.boxClass = "alert";
-	};
+	$scope.hideMini = ($scope.consumed == undefined || $scope.consumed.then(function(items) { return items.length == 0; }));
 
 	$scope.calculateCaloriesPercentage = function (calories) {
-	    return (parseFloat(calories) / parseFloat($scope.dailyCalories) * 100).toFixed(1);
+		return (parseFloat(calories) / parseFloat($scope.dailyCalories) * 100).toFixed(1);
 	};
 
 	/* Sets the colors for the bars related to nutritional values */
@@ -155,9 +164,7 @@ function TestController($scope, $location, Nutrition, Phridge) {
 		$scope.data[3].value = (carb > 100 ? 0 : 100 - carb);
 		$scope.data[4].value = (fat > 100 ? 100 : fat);
 		$scope.data[5].value = (fat > 100 ? 0 : 100 - fat);
-        	new Chart(document.getElementById('intakeChart').getContext('2d')).Doughnut($scope.data,{percentageInnerCutout: 50, segmentShowStroke: false});
-
-	    
+		new Chart(document.getElementById('intakeChart').getContext('2d')).Doughnut($scope.data,{percentageInnerCutout: 50, segmentShowStroke: false});
 		return '';
 	};
 
